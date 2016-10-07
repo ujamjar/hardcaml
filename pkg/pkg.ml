476be117e0b2bb200ab2210ce5a7117cf4c44c19
andrewray <evilkidder@gmail.com>
2016-10-07 16:28:48 +0100

    make js_of_ocaml optional

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,6 +5,7 @@ open Topkg
 
 let ctypes = Conf.with_pkg ~default:false "ctypes-foreign"
 let camlp4 = Conf.with_pkg ~default:false "camlp4"
+let js_of_ocaml = Conf.with_pkg ~default:false "js_of_ocaml"
 
 let mlpack ?(en=true) prefix = 
   if en then 
@@ -21,10 +22,11 @@ let () =
   Pkg.describe "hardcaml" @@ fun c ->
   let ctypes = Conf.value c ctypes in
   let camlp4 = Conf.value c camlp4 in
+  let js_of_ocaml = Conf.value c js_of_ocaml in
   Ok (
     mlpack "src/HardCaml" @
     mlpack "dynlink/HardCamlDynlink" @
-    mlpack "js/HardCamlJS" @
+    mlpack ~en:js_of_ocaml "js/HardCamlJS" @
     mlpack ~en:ctypes "csim/HardCamlCSim" @
     (if camlp4 then [ Pkg.lib "syntax/pa_hardcaml.cmo" ] else [])
   )
