d67e9afcfb73117852b8cc8a61bea1c4e60184df
andrewray <evilkidder@gmail.com>
2016-10-08 19:48:22 +0100

    add 4.03 to ppa matrix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,6 +11,7 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
 4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+4.03.0,1.2.0) ppa=avsm/ocaml43+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 	 
