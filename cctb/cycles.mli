bb8836dfaae06514410c3c8f89ab64b0d926e67e
andrewray <evilkidder@gmail.com>
2017-02-20 18:16:24 +0000

    add cycle based yield framework

--- cctb/cycles.mli
+++ cctb/cycles.mli
@@ -1,2 +1,8 @@
-val foo : unit -> unit
+type gen = [ `gen of ('a list -> unit) -> unit ] as 'a
+
+val delay : ?n:int -> gen -> gen
+
+val seq : gen list -> gen
+
+val run : ?n:int -> sim:'a HardCaml.Cyclesim.Api.cyclesim -> gen -> unit
 
